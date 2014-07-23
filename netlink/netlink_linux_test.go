2540a3d8c95d1cbba0f53c859c69c0b6886b4e16
Erik Hollensbe <github@hollensbe.org>
2014-07-23 15:34:25 -0700

    netlink/netlink_linux_test.go: tests for netlink add/rem ip
    Docker-DCO-1.1-Signed-off-by: Erik Hollensbe <github@hollensbe.org> (github: erikh)

--- netlink/netlink_linux_test.go
+++ netlink/netlink_linux_test.go
@@ -2,8 +2,65 @@ package netlink
 
 import (
 	"net"
+	"strings"
 	"testing"
 )
+
+func TestAddRemNetworkIp(t *testing.T) {
+	var err error
+
+	ifaceString := "lo"
+	ip := net.ParseIP("127.0.1.1")
+	mask := net.IPv4Mask(255, 255, 255, 255)
+	ipNet := &net.IPNet{ip, mask}
+
+	iface, err := net.InterfaceByName(ifaceString)
+	if err != nil {
+		t.Skip("No 'lo' interface; skipping tests")
+	}
+
+	err = NetworkLinkAddIp(iface, ip, ipNet)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var found bool
+
+	addrs, _ := iface.Addrs()
+
+	for _, addr := range addrs {
+		args := strings.SplitN(addr.String(), "/", 2)
+		if args[0] == ip.String() {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Fatalf("Could not locate address '%s' in lo address list.", ip.String())
+	}
+
+	err = NetworkLinkRemIp(iface, ip, ipNet)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	found = false
+
+	addrs, _ = iface.Addrs()
+
+	for _, addr := range addrs {
+		args := strings.SplitN(addr.String(), "/", 2)
+		if args[0] == ip.String() {
+			found = true
+			break
+		}
+	}
+
+	if found {
+		t.Fatal("Located address '%s' in lo address list after removal.", ip.String())
+	}
+}
 
 func TestCreateBridgeWithMac(t *testing.T) {
 	if testing.Short() {
