0305ada75a64f7eb78b9d936c7d7cf8a5795f96b
Ye Yin <hustcat@gmail.com>
2014-09-17 17:02:27 +0800

    set tx_queuelen to 0 when create veth device
    Signed-off-by: Ye Yin <hustcat@gmail.com>

--- netlink/netlink_linux.go
+++ netlink/netlink_linux.go
@@ -696,6 +696,11 @@ func NetworkCreateVethPair(name1, name2 string) error {
 	nameData := newRtAttr(syscall.IFLA_IFNAME, zeroTerminated(name1))
 	wb.AddData(nameData)
 
+	txqLen := make([]byte, 4)
+	native.PutUint32(txqLen, 0)
+	txqData := newRtAttr(syscall.IFLA_TXQLEN, txqLen)
+	wb.AddData(txqData)
+
 	nest1 := newRtAttr(syscall.IFLA_LINKINFO, nil)
 	newRtAttrChild(nest1, IFLA_INFO_KIND, zeroTerminated("veth"))
 	nest2 := newRtAttrChild(nest1, IFLA_INFO_DATA, nil)
@@ -703,6 +708,10 @@ func NetworkCreateVethPair(name1, name2 string) error {
 
 	newIfInfomsgChild(nest3, syscall.AF_UNSPEC)
 	newRtAttrChild(nest3, syscall.IFLA_IFNAME, zeroTerminated(name2))
+
+	txqLen2 := make([]byte, 4)
+	native.PutUint32(txqLen2, 0)
+	newRtAttrChild(nest3, syscall.IFLA_TXQLEN, txqLen2)
 
 	wb.AddData(nest1)
 
