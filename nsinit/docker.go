543d27c382bb050ef0660c3541e11f0ad9be06b8
Mrunal Patel <mrunalp@gmail.com>
2015-04-07 13:09:02 -0400

    nsinit: Helper functions for loading docker container state/config
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

---
+++ nsinit/docker.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+
+	"github.com/codegangsta/cli"
+	"github.com/docker/libcontainer"
+	"github.com/docker/libcontainer/cgroups/systemd"
+	"github.com/docker/libcontainer/configs"
+)
+
+const (
+	dockerRoot    = "/var/run/docker/execdriver/native"
+	stateFilename = "state.json"
+)
+
+func loadDockerFactory(context *cli.Context) (libcontainer.Factory, error) {
+	cgm := libcontainer.Cgroupfs
+	if systemd.UseSystemd() {
+		cgm = libcontainer.SystemdCgroups
+	}
+	return libcontainer.New(dockerRoot, cgm)
+}
+
+func loadState(root string) (*libcontainer.State, error) {
+	f, err := os.Open(filepath.Join(root, stateFilename))
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, err
+		}
+		return nil, err
+	}
+	defer f.Close()
+	var state *libcontainer.State
+	if err := json.NewDecoder(f).Decode(&state); err != nil {
+		return nil, err
+	}
+	return state, nil
+}
+
+func loadDockerConfig(context *cli.Context) (*configs.Config, error) {
+	containerRoot := filepath.Join(dockerRoot, context.String("id"))
+	state, err := loadState(containerRoot)
+	if err != nil {
+		return nil, err
+	}
+	config := &state.Config
+	modify(config, context)
+	return config, nil
+}
