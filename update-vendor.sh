c9ec1dc53cc28878bfb689d35ad75940448c39ff
Alexander Morozov <lk4d4@docker.com>
2015-06-19 16:58:29 -0700

    Vendor github.com/vishvananda/netlink
    Signed-off-by: Alexander Morozov <lk4d4@docker.com>

--- update-vendor.sh
+++ update-vendor.sh
@@ -46,5 +46,6 @@ clone git github.com/godbus/dbus v2
 clone git github.com/Sirupsen/logrus v0.7.3
 clone git github.com/syndtr/gocapability 66ef2aa
 clone git github.com/golang/protobuf 655cdfa588ea
+clone git github.com/vishvananda/netlink b1aa0245acaf634bf2c907c5850676a6e0aa77d6
 
 # intentionally not vendoring Docker itself...  that'd be a circle :)
